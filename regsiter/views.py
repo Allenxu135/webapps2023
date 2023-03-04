6554e8853a5173f75dd1bda1166cbab6641ff15f
ChenyiXu(Allen) <allen_xu135@163.com>
2023-03-04 03:36:04 +0000

    still have errror on it

--- regsiter/views.py
+++ regsiter/views.py
@@ -1,113 +1,107 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, DetailView
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.contrib import messages
-from .models import UserProfile, Payment, PaymentRequest
+from django.db.models import Q
+from decimal import Decimal
+from .models import UserProfile, PaymentRequest, Payment
 
 
-@login_required
-def dashboard(request):
-    user_profile = UserProfile.objects.get(user=request.user)
-    context = {
-        'balance': user_profile.balance,
-        'selected_currency': user_profile.selected_currency,
-        'sent_payments': user_profile.sent_payments.all(),
-        'received_payments': user_profile.received_payments.all(),
-        'sent_requests': user_profile.sent_requests.all(),
-        'received_requests': user_profile.received_requests.all(),
-    }
-    return render(request, 'dashboard.html', context)
+class HomePageView(TemplateView):
+    """
+    Home page view that displays a welcome message.
+    """
+    template_name = 'webapps2023/home.html'
 
 
-@login_required
-def initiate_payment(request):
+class SignUpView(CreateView):
+    """
+    Sign up view that allows new users to register.
+    """
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'webapps2023/signup.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class UserProfileView(TemplateView):
+    """
+    User profile view that displays the authenticated user's profile.
+    """
+    template_name = 'webapps2023/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_profile'] = UserProfile.objects.get(user=self.request.user)
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class PaymentRequestView(CreateView):
+    """
+    Payment request view that allows users to create payment requests.
+    """
+    model = PaymentRequest
+    fields = ['recipient', 'amount', 'currency']
+    template_name = 'webapps2023/payment_request.html'
+    success_url = reverse_lazy('payment_request_list')
+
+    def form_valid(self, form):
+        # Set the sender profile to the authenticated user's profile
+        form.instance.sender = UserProfile.objects.get(user=self.request.user)
+        return super().form_valid(form)
+
+
+@method_decorator(login_required, name='dispatch')
+class PaymentRequestListView(TemplateView):
+    """
+    Payment request list view that displays a list of payment requests for the authenticated user.
+    """
+    template_name = 'webapps2023/payment_request_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get the authenticated user's profile and their payment requests
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        payment_requests = PaymentRequest.objects.filter(Q(sender=user_profile) | Q(recipient=user_profile))
+        context['payment_requests'] = payment_requests
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+def accept_payment_request(request, request_id):
+    """
+    Accept payment request view that allows users to accept payment requests.
+    """
+    payment_request = get_object_or_404(PaymentRequest, id=request_id)
     if request.method == 'POST':
-        recipient_username = request.POST['recipient_username']
-        amount = float(request.POST['amount'])
-        currency = request.POST['currency']
-        recipient_user = get_object_or_404(UserProfile, user__username=recipient_username)
-        sender_profile = UserProfile.objects.get(user=request.user)
-        recipient_profile = UserProfile.objects.get(user=recipient_user.user)
-        if sender_profile.balance >= amount:
-            Payment.objects.create(sender=sender_profile, recipient=recipient_profile, amount=amount, currency=currency)
-            sender_profile.balance -= amount
-            sender_profile.save()
-            recipient_profile.balance += amount
-            recipient_profile.save()
-            messages.success(request, f'Payment of {amount} {currency} was successfully sent to {recipient_username}.')
-        else:
-            messages.error(request, 'Insufficient funds.')
-    return redirect('dashboard')
-
-
-@login_required
-def accept_payment_request(request, pk):
-    payment_request = get_object_or_404(PaymentRequest, pk=pk)
-    if payment_request.recipient.user != request.user:
-        messages.error(request, 'You are not authorized to perform this action.')
-        return redirect('dashboard')
-    if payment_request.is_pending:
-        sender_profile = payment_request.sender
-        recipient_profile = payment_request.recipient
-        amount = payment_request.amount
-        currency = payment_request.currency
-        if recipient_profile.balance >= amount:
-            Payment.objects.create(sender=sender_profile, recipient=recipient_profile, amount=amount, currency=currency)
-            recipient_profile.balance -= amount
-            recipient_profile.save()
-            sender_profile.balance += amount
-            sender_profile.save()
-            payment_request.is_pending = False
-            payment_request.save()
-            messages.success(request, f'Payment of {amount} {currency} was successfully accepted.')
-        else:
-            messages.error(request, 'Insufficient funds.')
+        # Create a new payment object
+        Payment.objects.create(sender=payment_request.sender, recipient=payment_request.recipient,
+                               amount=payment_request.amount, currency=payment_request.currency)
+        # Delete the payment request object
+        payment_request.delete()
+        messages.success(request, 'Payment request accepted successfully.')
+        return redirect('payment_request_list')
     else:
-        messages.error(request, 'This payment request has already been accepted or rejected.')
-    return redirect('dashboard')
+        return render(request, 'webapps2023/payment_request_accept.html', {'payment_request': payment_request})
+
+@method_decorator(login_required, name='dispatch')
+class PaymentListView(TemplateView):
+    """
+    Payment list view that displays a list of payments for the authenticated user.
+    """
+    template_name = 'webapps2023/payment_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get the authenticated user's profile and their payments
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        payments = Payment.objects.filter(Q(sender=user_profile) | Q(recipient=user_profile))
+        context['payments'] = payments
+        return context
 
 
-@login_required
-def reject_payment_request(request, pk):
-    payment_request = get_object_or_404(PaymentRequest, pk=pk)
-    if payment_request.recipient.user != request.user:
-        messages.error(request, 'You are not authorized to perform this action.')
-        return redirect('dashboard')
-    if payment_request.is_pending:
-        payment_request.is_pending = False
-        payment_request.save()
-        messages.success(request, 'Payment request was successfully rejected.')
-    else:
-        messages.error(request, 'This payment request has already been accepted or rejected.')
-    return redirect('dashboard')
-
-
-class PaymentRequestForm:
-    pass
-
-
-@login_required
-def make_payment_request(request):
-    if request.method == 'POST':
-        form = PaymentRequestForm(request.POST)
-        if form.is_valid():
-            recipient_username = form.cleaned_data['recipient_username']
-            amount = form.cleaned_data['amount']
-            currency = form.cleaned_data['currency']
-            recipient_profile = get_object_or_404(UserProfile, user__username=recipient_username)
-            sender_profile = UserProfile.objects.get(user=request.user)
-
-            if sender_profile.balance < amount:
-                messages.error(request, 'Insufficient funds')
-                return redirect('make_payment_request')
-
-            payment_request = PaymentRequest(sender=sender_profile, recipient=recipient_profile, amount=amount, currency=currency)
-            payment_request.save()
-
-            messages.success(request, 'Payment request sent')
-            return redirect('view_payment_requests')
-
-    else:
-        form = PaymentRequestForm()
-
-    return render(request, 'register/payment_request.html', {'form': form})
 
--- webapps2023/urls.py
+++ webapps2023/urls.py
@@ -15,18 +15,22 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from webapps2023 import views
-
-app_name = 'webapp'
+from .views import HomePageView, SignUpView, UserProfileView, PaymentRequestView, PaymentRequestListView, \
+    accept_payment_request, PaymentListView, initiate_payment, accept_payment, reject_payment, make_payment_request
 
 urlpatterns = [
-    path('webapps2023/login/', auth_views.LoginView.as_view(template_name='webapp/login.html'), name='login'),
-    path('webapps2023/logout/', auth_views.LogoutView.as_view(next_page='/webapps2023/login/'), name='logout'),
-    path('webapps2023/signup/', views.SignUpView.as_view(), name='signup'),
-    path('webapps2023/profile/', views.ProfileView.as_view(), name='profile'),
-    path('webapps2023/make-payment-request/', views.make_payment_request, name='make_payment_request'),
-    path('webapps2023/view-payment-requests/', views.view_payment_requests, name='view_payment_requests'),
-    path('webapps2023/accept-payment-request/<int:request_id>/', views.accept_payment_request, name='accept_payment_request'),
-    path('webapps2023/admin/', views.admin_panel, name='admin_panel'),
-    path('webapps2023/admin/edit-user/<int:user_id>/', views.edit_user, name='edit_user')
+    path('webapps2023/', HomePageView.as_view(), name='home'),
+    path('webapps2023/signup/', SignUpView.as_view(), name='signup'),
+    path('webapps2023/login/', auth_views.LoginView.as_view(template_name='webapps2023/login.html'), name='login'),
+    path('webapps2023/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('webapps2023/profile/', UserProfileView.as_view(), name='profile'),
+    path('webapps2023/payment-request/', PaymentRequestView.as_view(), name='payment_request'),
+    path('webapps2023/payment-request-list/', PaymentRequestListView.as_view(), name='payment_request_list'),
+    path('webapps2023/accept-payment-request/<int:request_id>/', accept_payment_request, name='accept_payment_request'),
+    path('webapps2023/payment-list/', PaymentListView.as_view(), name='payment_list'),
+    path('webapps2023/initiate-payment/', initiate_payment, name='initiate_payment'),
+    path('webapps2023/accept-payment/<int:pk>/', accept_payment, name='accept_payment'),
+    path('webapps2023/reject-payment/<int:pk>/', reject_payment, name='reject_payment'),
+    path('webapps2023/make-payment-request/', make_payment_request, name='make_payment_request'),
 ]
+
