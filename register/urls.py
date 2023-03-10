a863f6b9b9e2c7fae7c15b4974bd74378b6ee89c
ChenyiXu(Allen) <allen_xu135@163.com>
2023-03-10 20:09:06 +0000

    the home have two button to access, but signup function can not be applied ,need to fix

--- register/urls.py
+++ register/urls.py
@@ -6,13 +6,13 @@ from django.contrib.auth import views as auth_views
 app_name = 'register'
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='webapps2023/home'),
-    path('signup/', SignUpView.as_view(), name='webapps2023/signup'),
-    path('profile/', UserProfileView.as_view(), name='webapps2023/profile'),
+    path('', HomePageView.as_view(), name='webapps2023/home.html'),
+    path('signup/', SignUpView.as_view(), name='webapps2023/sign up.html'),
+    path('profile/', UserProfileView.as_view(), name='webapps2023/profile.html'),
     path('payment_request/', PaymentRequestView.as_view(), name='webapps2023/payment_request'),
-    path('payment_request_list/', PaymentRequestListView.as_view(), name='webapps2023/payment_request_list'),
-    path('accept_payment_request/<int:request_id>/', accept_payment_request, name='webapps2023/accept_payment_request'),
-    path('payment_list/', PaymentListView.as_view(), name='webapps2023/payment_list'),
+    path('payment_request_list/', PaymentRequestListView.as_view(), name='webapps2023/payment_request_list.html'),
+    path('accept_payment_request/<int:request_id>/', accept_payment_request, name='webapps2023/accept_payment_request.html'),
+    path('payment_list/', PaymentListView.as_view(), name='webapps2023/payment_list.html'),
     path('login/', auth_views.LoginView.as_view(template_name='webapps2023/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='webapps2023/logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='webapps2023/logout.html'),
 ]
